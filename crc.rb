a85f0ccd24c4814eaa11c0cb3e7f3c69a44d0a50
Trejkaz <trejkaz@trypticon.org>
2013-10-09 00:45:29 +1100

    Faster CRC

--- crc.rb
+++ crc.rb
@@ -1,5 +1,6 @@
 class CRC
   def initialize
+    @table = make_table(0x04C11DB7)
     reset
   end
 
@@ -9,21 +10,39 @@ class CRC
 
   def update(input)
     crc = @crc
+    table = @table
+
     input.unpack('V*').each do |data|
-      32.times do
-        if ((data ^ crc) & 0x80000000) != 0
-          crc = ((crc << 1) ^ 0x04C11DB7) & 0xFFFFFFFF
-        else
-          crc = (crc << 1)
-        end
-        data = (data << 1) & 0xFFFFFFFF
+      4.times do
+        table_index = (data >> 24) ^ (crc >> 24)
+        crc = (crc << 8) & 0xFFFFFFFF
+        crc = crc ^ table[table_index]
+        data = (data << 8) & 0xFFFFFFFF
       end
     end
+
     @crc = crc
   end
 
   def crc
     @crc
   end
+
+private
+
+  def make_table(poly)
+    (0..255).map do |n|
+      crc = n << 24
+      8.times do
+        if crc & 0x80000000 != 0
+          crc = ((crc << 1) ^ poly) & 0xFFFFFFFF
+        else
+          crc <<= 1
+        end
+      end
+      (crc & 0xFFFFFFFF)
+    end
+  end
+
 end
 
