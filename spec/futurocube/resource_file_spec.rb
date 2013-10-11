ecfa7a77b9e43236f1b6189ecd7991bfaaa212a8
Trejkaz <trejkaz@trypticon.org>
2013-10-12 01:46:07 +1100

    Specs for the structures inside ResourceFile

--- spec/futurocube/resource_file_spec.rb
+++ spec/futurocube/resource_file_spec.rb
@@ -11,8 +11,46 @@ describe FuturoCube::ResourceFile do
   end
 
   describe '#header' do
-    it 'returns an instance of FileHeader' do
-      @rf.header.should be_a_kind_of(FuturoCube::ResourceFile::FileHeader)
+    describe '#magic' do
+      it 'returns the magic number' do
+        @rf.header.magic.should == 0xAA987745
+      end
+    end
+    
+    describe '#checksum' do
+      it 'returns the file checksum' do
+        @rf.header.checksum.should == 0x35D806F6
+      end
+    end
+    
+    describe '#version' do
+      it 'returns the file format version' do
+        @rf.header.version.should == 2
+      end
+    end
+
+    describe '#created' do
+      it 'returns the file creation date' do
+        @rf.header.created.should == 1354536121
+      end
+    end
+
+    describe '#name' do
+      it 'returns the resource bundle name' do
+        @rf.header.name.should == 'FutRes031212'
+      end
+    end
+
+    describe '#record_count' do
+      it 'returns the record count' do
+        @rf.header.record_count.should == 274
+      end
+    end
+    
+    describe '#file_size' do
+      it 'returns the file size' do
+        @rf.header.file_size.should == 98828288
+      end
     end
   end
 
@@ -21,8 +59,28 @@ describe FuturoCube::ResourceFile do
       @rf.records.size.should == 274
     end
 
-    it 'returns an array of ResourceRecord' do
-      @rf.records[0].should be_a_kind_of(FuturoCube::ResourceFile::ResourceRecord)
+    describe '#name' do
+      it 'returns the name of the record' do
+        @rf.records[0].name.should == '1'
+      end
+    end
+
+    describe '#format' do
+      it 'returns a format version number' do
+        @rf.records[0].format.should == 1
+      end
+    end
+    
+    describe '#data_offset' do
+      it 'returns the data offset' do
+        @rf.records[0].data_offset.should == 10240
+      end
+    end
+    
+    describe '#data_length' do
+      it 'returns the data length' do
+        @rf.records[0].data_length.should == 29184
+      end
     end
   end
 
