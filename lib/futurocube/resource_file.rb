6f18ef61955f9f4d8f206ddbcd31571c7cdccedc
Trejkaz <trejkaz@trypticon.org>
2013-10-12 01:21:37 +1100

    Fixing the namespace being wrong; also reusing the buffer when checksumming

--- lib/futurocube/resource_file.rb
+++ lib/futurocube/resource_file.rb
@@ -1,91 +1,99 @@
 require 'bindata'
 
-class ResourceFile
-  class FileHeader < BinData::Record
-    endian :little
-    uint32 :magic
-    uint32 :checksum
-    uint32 :version
-    uint32 :created
-    string :name, :length => 20, :trim_padding => true
-    uint32 :record_count
-    uint32 :file_size
-  end
-
-  class ResourceRecord < BinData::Record
-    endian :little
-    string :name, :length => 20, :trim_padding => true
-    uint32 :format
-    uint32 :data_offset
-    uint32 :data_length
-  end
-
-  attr_reader :header, :records
-
-  def initialize(io)
-    @io = io
-  end
-
-  # Reads the header from the resource file.
-  def header
-    if !@header
-      @io.seek(0, IO::SEEK_SET)
-      @header = FileHeader.read(@io)
+module FuturoCube
+  class ResourceFile
+    class FileHeader < BinData::Record
+      endian :little
+      uint32 :magic
+      uint32 :checksum
+      uint32 :version
+      uint32 :created
+      string :name, :length => 20, :trim_padding => true
+      uint32 :record_count
+      uint32 :file_size
     end
-    @header
-  end
 
-  # Reads the list of records from the resource file.
-  def records
-    if !@records
-      @io.seek(44, IO::SEEK_SET)
-      records = []
-      header.record_count.times do
-        records << ResourceRecord.read(@io)
+    class ResourceRecord < BinData::Record
+      endian :little
+      string :name, :length => 20, :trim_padding => true
+      uint32 :format
+      uint32 :data_offset
+      uint32 :data_length
+    end
+
+    attr_reader :header, :records
+
+    def initialize(io)
+      @io = io
+    end
+
+    # Reads the header from the resource file.
+    def header
+      if !@header
+        @io.seek(0, IO::SEEK_SET)
+        @header = FileHeader.read(@io)
       end
-      @records = records
+      @header
     end
-    @records
-  end
 
-  # Reads the data for a given record.
-  #
-  # @param rec [ResourceRecord] the record to read the data for.
-  def data(rec)
-    @io.seek(rec.data_offset)
-    @io.read(rec.data_length)
-  end
-
-  # Computes the checksum for the resource file.
-  #
-  # @yield [done] Provides feedback about the progress of the operation.
-  def compute_checksum(&block)
-    crc = CRC.new
-    @io.seek(8)
-    pos = 8
-    length = 4096-8
-    file_size = header.file_size
-    while length > 0
-      crc.update(@io.read(length))
-      pos += length
-      length = [4096, file_size-pos].min
-      block.call(pos) if block
+    # Reads the list of records from the resource file.
+    def records
+      if !@records
+        @io.seek(44, IO::SEEK_SET)
+        records = []
+        header.record_count.times do
+          records << ResourceRecord.read(@io)
+        end
+        @records = records
+      end
+      @records
     end
-    crc.crc
-  end
 
-  def self.open(file, &block)
-    File.open(file, File::RDONLY) do |io|
-      rf = ResourceFile.new(io)
-      begin
-        block.call(rf)
-      ensure
-        rf.close
+    # Reads the data for a given record.
+    #
+    # @param rec [ResourceRecord] the record to read the data for.
+    def data(rec)
+      @io.seek(rec.data_offset, IO::SEEK_SET)
+      @io.read(rec.data_length)
+    end
+
+    # Computes the checksum for the resource file.
+    #
+    # @yield [done] Provides feedback about the progress of the operation.
+    def compute_checksum(&block)
+      crc = CRC.new
+      @io.seek(8, IO::SEEK_SET)
+      pos = 8
+      length = 4096-8
+      file_size = header.file_size
+      buf = nil
+      while length > 0
+        buf = @io.read(length, buf)
+        crc.update(buf)
+        pos += length
+        length = [4096, file_size-pos].min
+        block.call(pos) if block
+      end
+      crc.crc
+    end
+
+    def self.open(file, &block)
+      if block
+        File.open(file, File::RDONLY) do |io|
+          rf = ResourceFile.new(io)
+          begin
+            block.call(rf)
+          ensure
+            rf.close
+          end
+        end
+      else
+        ResourceFile.new(File.open(file, File::RDONLY))
       end
     end
-  end
 
-  def close
-    @io.close
+    def close
+      @io.close
+    end
   end
 end
