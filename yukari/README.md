8d2c29298a5c57016b0e7441dd8c073372e0d92f
Trejkaz (pen name) <trejkaz@trypticon.org>
2015-09-13 23:31:47 +1000

    Preliminary Ruby script to run VoiceroidEx for each string

--- yukari/README.md
+++ yukari/README.md
@@ -11,9 +11,11 @@ Progress
 --------
 
 * TODO - Get text translated into Japanese
+    * TODO - Make a script file containing all the English lines
+    * TODO - Get the translation itself done
 * TODO - Automate VOICEROID to spit out WAV files
     * **DONE** - Acquire technology - [VoiceroidEx](https://github.com/saga-dash/VoiceroidEx) has bugs but I have fixed or worked around them
-    * TODO - Write scripts (easy!)
+    * **DONE** - Write script to spit out WAV files
 * TODO - Automate conversion of WAV files into whatever bitrate will fit on the cube
     * TODO - Decide whether to write it (easy) or script some existing tool (easy)
     * TODO - Write scripts
---
+++ yukari/build.rb
@@ -0,0 +1,22 @@
+require 'fileutils'
+
+VOICEROIDEX_EXE = File.expand_path('~/Documents/GitHub/VoiceroidEx/VoiceroidEx/bin/Debug/VoiceroidEx.exe')
+OUTPUT_DIR = File.join(File.expand_path(File.dirname(__FILE__)), 'output')
+
+FileUtils.mkdir_p(OUTPUT_DIR)
+
+File.readlines('script.txt', encoding: 'UTF-8').each do |line|
+  line.chomp!.strip!
+  next if line =~ /^#/
+  if line =~ /^(.*?)\s+=\s+(.*?)$/
+    filename = $1
+    text = $2
+
+    # Workaround for VOICEROID+ bug, can't handle multiple saves going at once.
+    sleep 1
+
+    if !system(VOICEROIDEX_EXE, text, File.join(OUTPUT_DIR, filename).gsub('/', "\\"))
+      raise "Failed for line: #{line}"
+    end
+  end
+end
